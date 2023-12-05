cf3b07f4a9ec00e50a46572e6b1ca7bd74aa6c11
27dCnc <143036993+nwt-q@users.noreply.github.com>
2023-12-05 23:28:02 +0800

    Update DFS.cpp
    Signed-off-by: 27dCnc <143036993+nwt-q@users.noreply.github.com>

--- DFS.cpp
+++ DFS.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 int dfs(int arr[45][45],int ex,int ey,int cx,int cy){//DFS模版
     if(cx==ex&&cy==ey)return 1;  //如果到达终点
     int ans = 0;
@@ -20,9 +19,8 @@ int main(){
     int a,b,c,d;
     scanf("%d %d %d %d",&a,&b,&c,&d);
     int arr[45][45];
-    long long step[45][45];
     memset(arr,0,sizeof(arr));
-    a+=1,b+=1,c+=1,d+=1;
+    a+=2,b+=2,c+=2,d+=2;
     arr[c][d]=1;
     arr[c+2][d+1]=1;
     arr[c+1][d+2]=1;
@@ -32,11 +30,6 @@ int main(){
     arr[c-1][d-2]=1;
     arr[c+1][d-2]=1;
     arr[c+2][d-1]=1;
-    step[1][1]=1;
-    for(int i=1;i<=a;i++)
-		for(int j=1;j<=b;j++) 
-            if((i!=1||j!=1)&&arr[i][j]!=1) 
-                step[i][j]=step[i-1][j]+step[i][j-1];//不能走进马的控制点
-    cout<<step[a][b];
+    cout<<dfs(arr,a,b,2,2);
 }
 //https://www.luogu.com.cn/problem/P1002
