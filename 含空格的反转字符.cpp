4924547b4d3cabe90e9b84078d3296eaff374d78
27dCnc <143036993+nwt-q@users.noreply.github.com>
2023-12-11 21:57:01 +0800

    Create 含空格的反转字符.cpp
    Signed-off-by: 27dCnc <143036993+nwt-q@users.noreply.github.com>

---
+++ 含空格的反转字符.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+//1
+int main(){
+    char b[100];  
+    std::scanf("%[^\n]s", b);
+    string s;
+    s.assign(b,strlen(b));
+    cout<<s.substr(0,6)<<"\n";
+    reverse(s.begin(),s.end());
+    cout<<s;
+}
+
+//服考试的时候就是想不到GTP?
+
+
+//2
+// int main() {  
+//     char b[100];  
+//     std::scanf("%[^\n]s", b);  // 读取直到遇到换行符或字符串结束  
+//     // s.assign(b, strlen(b));
+//     // std::string c(b, b + strlen(b));  // 复制b到c，为了反转  //复制的时候会将空格读取
+//     std::reverse(c.begin(), c.end());  // 反转字符串c  
+
+//     std::cout << c << std::endl;  // 打印反转后的字符串并添加换行符  
+//     return 0;  
+// }
+
+//3
+// int main(){
+//     char b[100];
+//     scanf("%[^\n]s",&b);
+//     // string s(b);
+//     // cout<<s;
+//     // reverse(s.begin(),s.end());
+//     // s[strlen(b)]='\0';
+//     char c[1000];
+//     for(int i=strlen(b)-1,j=0;i>=0;i--,j++){
+        
+//         c[j]=b[i];
+//     }
+//     c[strlen(b)]='\0';
+//     // cout<<s;
+//     printf("%s",c);
+// }
