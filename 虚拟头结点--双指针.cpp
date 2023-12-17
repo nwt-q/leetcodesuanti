1f84470ec69294e6d44a90bd9c465a667deb9d56
27dCnc <143036993+nwt-q@users.noreply.github.com>
2023-12-17 23:45:22 +0800

    Create 虚拟头结点--双指针.cpp
    Signed-off-by: 27dCnc <143036993+nwt-q@users.noreply.github.com>

---
+++ 虚拟头结点--双指针.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode*fast = head;
+        ListNode*slow = head;
+        //我们彼此相识共同进步
+        while(fast!=nullptr&&fast->next!=nullptr){
+            //即使身处两个世界，以不相同的速度双向奔赴,但只要在同一恋爱循环中，就一定会相遇。
+            slow = slow->next;
+            fast = fast->next->next;
+            //在命运的某一时刻我们登入的婚姻的殿堂
+            if(slow==fast){
+                //在我们相遇的哪个时刻脑中回忆着过往
+                ListNode*cnt = slow; 
+                ListNode*cur = head;
+                while(cur!=cnt){
+                    //从相识到相知彼此断靠近
+                    cur=cur->next;
+                    cnt = cnt->next;
+                }
+                //直到回忆道我们的青涩告白返回我们当初最真挚的情感
+                return cur;
+            }
+        }
+        //尽管我们没有进入恋爱的循环我们都有着彼此的故事
+        return nullptr;
+    }
+};
