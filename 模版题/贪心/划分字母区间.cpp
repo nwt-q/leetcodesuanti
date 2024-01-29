fa9dc6b1882837941df17fa01519985a6ec70eb2
27dCnc <143036993+nwt-q@users.noreply.github.com>
2024-01-30 00:42:12 +0800

    Create 划分字母区间.cpp
    Signed-off-by: 27dCnc <143036993+nwt-q@users.noreply.github.com>

---
+++ 模版题/贪心/划分字母区间.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string S) {
+        int hash[27] = {0}; // i为字符，hash[i]为字符出现的最后位置
+        for (int i = 0; i < S.size(); i++) { // 统计每一个字符最后出现的位置
+            hash[S[i] - 'a'] = i;
+        }
+        vector<int> ans;
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < S.size(); i++) {
+            right = max(right, hash[S[i] - 'a']); // 找到字符出现的最远边界
+            if (i == right) {
+                ans.push_back(right - left + 1);
+                left = i + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+//也是划分没有重复的区间
+
+//https://leetcode.cn/problems/partition-labels/
